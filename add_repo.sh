29f4deed346fbde9c471ef41f1128a06ab04abed
larin <ser-ufl@mail.ru>
2026-01-18 01:29:40 +0300

    фиксы для минифицированного json

--- add_repo.sh
+++ add_repo.sh
@@ -66,7 +66,7 @@ if [ -z "$INSTALLED_VERSION" ]; then
     echo "Package not installed. Installing..."
     NEED_UPDATE=1
 else
-    LATEST_VERSION=$(wget -qO- https://api.github.com/repos/LarinIvan/MagiTrickle_Mod/releases/latest | grep '"tag_name"' | head -n1 | cut -d'"' -f4)
+    LATEST_VERSION=$(wget -qO- -U "MagiTrickle" https://api.github.com/repos/LarinIvan/MagiTrickle_Mod/releases/latest | awk -F'"' '{for(i=1;i<=NF;i++)if($i=="tag_name"){print $(i+2);exit}}')
     
     CLEAN_INSTALLED_VERSION=$(echo "$INSTALLED_VERSION" | sed 's/-[0-9]*$//')
 
